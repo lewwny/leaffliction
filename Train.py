02cafa5415fb84cdc86a20dcb2d06e05a6240c86
floppy727 <omarbhz@hotmail.com>
2026-02-02 17:36:45 +0100

    Finished data loader and model creator, WIP trining metrics

--- Train.py
+++ Train.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
-from torchvision import models, transforms
+from torchvision import models, transforms, datasets
 from pathlib import Path
 from PIL import Image, ImageEnhance
 from sklearn.model_selection import train_test_split
@@ -27,12 +27,15 @@ def print_device():
 
 
 def sort_data(folder: str):
+    """sorts data from folder for train and validation imgs"""
+    # get classes from source
     source = Path(folder)
     classes = [c.name for c in source.iterdir() if c.is_dir()]
     classes.sort()
 
     print(f"Classes Found: {len(classes)}")
 
+    # counts images
     total_imgs = 0
     for i, c in enumerate(classes):
         img_count = (len(list(source / c).glob("*.[jJ][pP][gG]"))
@@ -43,40 +46,98 @@ def sort_data(folder: str):
 
     print(f"Total images: {total_imgs}")
 
+    # rmtree for the temp
     temp_folder = Path("temp_dataset")
     if temp_folder.exists():
         shutil.rmtree(temp_folder)
 
+    # create train and validation directories
     train_dir = temp_folder / "train"
-    validation_dir = temp_folder / "validation"
+    valid_dir = temp_folder / "validation"
     train_dir.mkdir(parents=True, exist_ok=True)
-    validation_dir.mkdir(parents=True, exist_ok=True)
+    valid_dir.mkdir(parents=True, exist_ok=True)
 
+    # create subdirectories for classes in train/validation dirs
     for c in classes:
         (train_dir / c).mkdir(exist_ok=True)
-        (validation_dir / c).mkdir(exist_ok=True)
+        (valid_dir / c).mkdir(exist_ok=True)
 
         class_folder = source / c
 
+        # get using image type (jpg, jpeg, png) capitalization insensitive
         imgs = ((list(source / c).glob("*.[jJ][pP][gG]"))
                      + (list(source / c).glob("*.[jJ][pP][eE][gG]"))
                      + (list(source / c).glob("*.[pP][nN][gG]")))
 
-        train_imgs, valid_imgs = train_test_split(imgs, test_size=VALID_SPLIT)
+        # use sklearn train test split on images
+        train_imgs, valid_imgs = train_test_split(imgs, test_size=VALID_SPLIT, random_state=40)
+
+        # shutil copy all images in train and validation
+        for img in train_imgs:
+            shutil.copy(img, train_dir / c / img.name)
+        for img in valid_imgs:
+            shutil.copy(img, valid_dir / c / img.name)
+
+        print(f"{c}: {len(train_imgs)}\ntrain, {len(valid_imgs)} validation")
+
+    return train_dir, valid_dir, classes
+
+
+def data_load(train_dir, valid_dir):
+    """augmentation + normalization for training data"""
+    # transforming data for augmentation and norm
+    transforms_data = {
+        "train": transforms.Compose([transforms.RandomResizedCrop(IMG_SIZE[0]),
+                                     transforms.RandomHorizontalFlip(),
+                                    """...."""]),
+
+        "valid": transforms.Compose([transforms.Resize(256),
+                                     transforms.CenterCrop(IMG_SIZE[0]),
+                                    """...."""]),
+    }
+
+    # builds datasets from imagefolder
+    train_dataset = datasets.ImageFolder(train_dir, transforms_data["train"])
+    valid_dataset = datasets.ImageFolder(train_dir, transforms_data["valid"])
+
+    # create dataloader from datasets
+    train_loader = DataLoader(train_dataset, batch_size=B_SIZE, shuffle=True, num_workers=4)
+    valid_loader = DataLoader(valid_dataset, batch_size=B_SIZE, shuffle=True, num_workers=4)
+
+    return train_loader, valid_loader
+
+
+def create_model(classes):
+    """Creates a model using MobileNetV2"""
+    # gets the mobilenetv2 model with default weights
+    model = models.mobilenet_v2(weights=models.MobileNet_V2_Weights.DEFAULT)
+
+    # freeze base layers
+    for p in model.features.parameters():
+        p.requires_grad = False
+
+    # replace classifier
+    model.classifier[1] = nn.Linear(model.last_channel, classes)
+
+    return model.to(DEVICE)
 
 
 def main() -> int:
+    # argv for image folder
     if len(sys.argv) < 2:
         print("Usage: python Train.py <img_folder>")
         return 1
 
+    # check if folder exists
     img_folder = sys.argv[1]
     if not Path(img_folder).exists():
         print(f"Error: {img_folder} doesn't exist")
         return 1
 
-    print_method()
+    # show device used for training (either gpu or cpu)
+    print_device()
 
+    return 0
 
 if __name__ == "__main__":
     main()
