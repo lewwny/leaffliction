1b2776b8bda8b0e0ac7230cd1ce4efafc6d312ab
floppy727 <omarbhz@hotmail.com>
2026-02-02 16:26:34 +0100

    Working on sorting data for training

--- Train.py
+++ Train.py
@@ -1,14 +1,16 @@
-import json, zipfile, sys, shutil, cv2
+import json, zipfile, sys, shutil, cv2, os, hashlib, random, copy
 import numpy as np
 import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
 from torchvision import models, transforms
 from pathlib import Path
+from PIL import Image, ImageEnhance
+from sklearn.model_selection import train_test_split
 
 
 # consts
-METHOD = torch.device("gpu" if torch.cuda.is_available() else "cpu")
+DEVICE = torch.device("gpu" if torch.cuda.is_available() else "cpu")
 IMG_SIZE = (224, 244)
 B_SIZE = 32
 EPOCHS = 10
@@ -16,14 +18,15 @@ VALID_SPLIT = 0.2
 TARGET_ACC = 0.9
 
 
-def print_method():
-    if METHOD.type == "cuda":
+def print_device():
+    """prints device type for processing"""
+    if DEVICE.type == "cuda":
         print(f"GPU: {torch.cuda.get_device_name(0)}")
     else:
         print("Using CPU: Training will be slower")
 
 
-def organize_dataset(folder: str):
+def sort_data(folder: str):
     source = Path(folder)
     classes = [c.name for c in source.iterdir() if c.is_dir()]
     classes.sort()
@@ -40,6 +43,27 @@ def organize_dataset(folder: str):
 
     print(f"Total images: {total_imgs}")
 
+    temp_folder = Path("temp_dataset")
+    if temp_folder.exists():
+        shutil.rmtree(temp_folder)
+
+    train_dir = temp_folder / "train"
+    validation_dir = temp_folder / "validation"
+    train_dir.mkdir(parents=True, exist_ok=True)
+    validation_dir.mkdir(parents=True, exist_ok=True)
+
+    for c in classes:
+        (train_dir / c).mkdir(exist_ok=True)
+        (validation_dir / c).mkdir(exist_ok=True)
+
+        class_folder = source / c
+
+        imgs = ((list(source / c).glob("*.[jJ][pP][gG]"))
+                     + (list(source / c).glob("*.[jJ][pP][eE][gG]"))
+                     + (list(source / c).glob("*.[pP][nN][gG]")))
+
+        train_imgs, valid_imgs = train_test_split(imgs, test_size=VALID_SPLIT)
+
 
 def main() -> int:
     if len(sys.argv) < 2:
@@ -52,7 +76,6 @@ def main() -> int:
         return 1
 
     print_method()
-
 
 
 if __name__ == "__main__":
